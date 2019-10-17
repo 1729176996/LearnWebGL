5edb87560ddb98bacb9aabaad2b9f8a4a13e4702
1729176996 <1729176996@qq.com>
2019-10-17 14:37:53 +0800

    33

---
+++ js/index.js
@@ -0,0 +1,33 @@
+var mainData,loading;
+$(function(){
+    FastClick.attach(document.body);
+    mainData = new Vue({
+        el: "#main",
+        data:{
+            list:[],
+            bscroll:null
+        },
+        mounted:function(){
+            var _this = this;
+            
+            //渲染节点结束后执行
+            _this.$nextTick(function(){
+                if(_this.bscroll) _this.bscroll.destroy();
+                _this.bscroll = new BScroll(".content",{
+                    click: true,
+                    pullUpLoad: {
+                      threshold: 50
+                    }
+                })
+            })
+        },
+        methods:{
+            test1:function(){
+                window.location.href = 'test1.html';
+            },
+            submit:function(){
+                
+            }
+        }
+    })
+});
---
+++ js/test1.js
@@ -0,0 +1,18 @@
+var mainData,loading;
+$(function(){
+    FastClick.attach(document.body);
+    mainData = new Vue({
+        el: "#main",
+        data:{
+            
+        },
+        mounted:function(){
+            
+        },
+        methods:{
+            submit:function(){
+                
+            }
+        }
+    })
+});
